db5b254597f3996864af7a80cf4fea56c7c93afc
vishwashdark <vishsharma2003@gmail.com>
2024-06-13 21:13:48 +0530

    calculator homework

--- calculator.java
+++ calculator.java
@@ -1,3 +1,26 @@
+import java.util.*;
 public class calculator {
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number a");
+        int a = sc.nextInt();
+        System.out.println("enter number b");
+        int b = sc.nextInt();
+        System.out.println("What do you wanna do Press");
+        System.out.println("1 for addition");
+        System.out.println("2 for multiplication");
+        System.out.println("3 for division ");
+        System.out.println("4 for subraction");
+        int c= sc.nextInt();
+        switch(c){
+            case 1: System.out.println("addition"+(a+b));
+            break;
+            case 2: System.out.println("multiplication"+(a*b));
+            break;
+            case 3: System.out.println("Division"+a/b);
+            break;
+            case 4: System.out.println("subraction"+(a-b));
+            default: System.out.println("wrong input");
+        }
+    }
 }
