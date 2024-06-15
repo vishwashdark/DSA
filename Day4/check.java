69e428fe578bcfd5ff94f750a5636630d89b3c61
vishwashdark <vishsharma2003@gmail.com>
2024-06-15 21:55:39 +0530

    check

---
+++ Day4/check.java
@@ -0,0 +1,8 @@
+package Day4;
+
+public class check {
+    public static void main(String[] args) {
+        System.out.println(" a small break today but dont wanna break a streak so");
+    }
+    
+}
