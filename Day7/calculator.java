05b940004294be1217b9bcb93ffec62cbc1f13ac
vishwashdark <vishsharma2003@gmail.com>
2024-06-18 22:44:08 +0530

    product

--- Day7/calculator.java
+++ Day7/calculator.java
@@ -15,6 +15,12 @@ public class calculator {
         System.out.println("Subraction of two number is"+c);
         return;
     }
+    public static void product(int a,int b){
+        int c=0;
+        c=a*b;
+        System.out.println("multiplication of two numbers"+c);
+        return;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number a");
@@ -24,10 +30,14 @@ public class calculator {
         System.out.println("What do you wanna do ");
         System.out.println("1. for addition");
         System.out.println("2. subraction");
+        System.out.println("3 for multiplication");
         System.out.println("enter your choice");
         int c= sc.nextInt();
         if(c==1){
             add(a,b);
+        }
+        else if(c==3){
+            product(a, b);
         }
         else{
             subract(a,b);
