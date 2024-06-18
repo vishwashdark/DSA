de32db0d01744470c764197892cfc03ea53353bf
vishwashdark <vishsharma2003@gmail.com>
2024-06-18 22:58:58 +0530

    new changes

---
+++ Day7/greater.java
@@ -0,0 +1,12 @@
+public class greater {
+    public static void great(int a,int b){
+        if(a>b){
+            System.out.println("a is greater");
+        }else{
+            System.out.println("b is greater");
+        }
+    }
+    public static void main(String[] args) {
+        great(5,2);
+    }
+}
