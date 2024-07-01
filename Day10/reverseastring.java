5f14605d55ec45949835cb817e152ea6c18d0233
vishwashdark <vishsharma2003@gmail.com>
2024-07-01 19:17:22 +0530

    ulta string

---
+++ Day10/reverseastring.java
@@ -0,0 +1,9 @@
+public class reverseastring {
+    public static void main(String[] args) {
+        String s = "Naman";
+        for(int i=s.length()-1;i>=0;i--){
+            System.out.print(s.charAt(i));
+        }
+    }
+
+}
